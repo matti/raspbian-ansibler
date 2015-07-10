f241431b3b27e1b085f9fb7fda4d1cd28b441e2b
Matti Paksula <matti.paksula@iki.fi>
2015-07-10 19:33:44 +0300

    sudo

--- ansible_deps.sh
+++ ansible_deps.sh
@@ -4,4 +4,4 @@ PI_HOST=$1
 PI_PORT=$2
 
 ssh -p $PI_PORT root@$PI_HOST "apt-get update && apt-get upgrade -y"
-ssh -p $PI_PORT root@$PI_HOST "apt-get install -y python2.7 python-apt"
+ssh -p $PI_PORT root@$PI_HOST "apt-get install -y python2.7 python-apt sudo"
